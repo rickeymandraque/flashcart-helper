1d1ed0d84d6fe882e9dd743e2a52fb6583f48983
ronny6993 <ronny6993@gmail.com>
2012-06-01 00:00:55 +0000

    - 1 small aesthetic change

--- flashcarthelper/Menus/FHMenu.Designer.vb
+++ flashcarthelper/Menus/FHMenu.Designer.vb
@@ -53,7 +53,7 @@ Partial Class FHMenu
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(725, 12)
+        Me.Label1.Location = New System.Drawing.Point(820, 420)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(39, 13)
         Me.Label1.TabIndex = 2
